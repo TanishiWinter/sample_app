7cd98af22f4637d9b16425eb3c0c01917dcf3c8a
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-06-05 01:36:34 +0000

    [Finish] 3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,12 +6,14 @@ class ListsController < ApplicationController
 
   def create
      # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
+    @list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    #削除redirect_to '/top'
-    redirect_to list_path(list.id)
+    if @list.save
+      redirect_to list_path(list.id)
+    else
+      render :new
+    end
+
   end
 
   def index
@@ -35,7 +37,7 @@ class ListsController < ApplicationController
   def destroy
     list = List.find(params[:id])  # データ（レコード）を1件取得
     list.destroy  # データ（レコード）を削除
-    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
   private
